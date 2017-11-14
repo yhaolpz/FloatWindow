c5543a1bd9a632e0f04292143d24d2265bf22886
yhaowa <yhaowa@outlook.com>
2017-11-14 23:54:22 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # FixedFloatWindow
 Andorid 任意界面悬浮窗，适配 4.x~7.1 及各大国产机型，无需申请权限
+
+
+特性：
+===
+
+1.可在应用内任意界面及桌面显示
+
+2.Andorid 7.1 版本以下无需申请权限
+
+3.适配各大国产机型
+
+4.内部封装权限申请操作
+
+5.简易、轻量
+
+6.位置不可变、Andorid 4.4 以下无法接收触摸事件
+
+
+集成：
+===
+
+第 1 步、在工程的 build.gradle 中添加：
+
+```
+	allprojects {
+		repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+第 2 步、在应用的  build.gradle 中添加：
+
+```
+	dependencies {
+	        compile 'com.github.yhaolpz:FixedFloatWindow:1.0.2'
+	}
+```
+
+使用：
+===
+
+```java
+
+    Button button = new Button(getApplicationContext());
+    button.setText("悬浮按钮");
+    button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+
+    FixedFloatWindow fixedFloatWindow = new FixedFloatWindow(getApplicationContext());
+    fixedFloatWindow.setView(button);
+    fixedFloatWindow.setGravity(Gravity.RIGHT | Gravity.TOP, 100, 150);
+    fixedFloatWindow.show();
+//   fixedFloatWindow.hide();
+```
+
+
+效果：
+===
+
+![悬浮按钮图](https://raw.githubusercontent.com/yhaolpz/SlideAdapter/master/img.jpg)
--- sample/src/main/java/com/example/yhao/fixedfloatwindow/MainActivity.java
+++ sample/src/main/java/com/example/yhao/fixedfloatwindow/MainActivity.java
@@ -1,33 +1,31 @@
 package com.example.yhao.fixedfloatwindow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.fixedfloatwindow.FixedFloatWindow;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final FixedFloatWindow fixedFloatWindow = new FixedFloatWindow(getApplicationContext());
-        Button button = new Button(getApplicationContext());
-        button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "onClick", Toast.LENGTH_SHORT).show();
-                fixedFloatWindow.hide();
-            }
-        });
 
+        Button button = new Button(getApplicationContext());
+        button.setText("悬浮按钮");
+        button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+
+        FixedFloatWindow fixedFloatWindow = new FixedFloatWindow(getApplicationContext());
         fixedFloatWindow.setView(button);
         fixedFloatWindow.setGravity(Gravity.RIGHT | Gravity.TOP, 100, 150);
         fixedFloatWindow.show();
+//        fixedFloatWindow.hide();
     }
+
+
+
 }
